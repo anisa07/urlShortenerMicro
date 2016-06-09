7b3dd317b77c9c85798659dfef5ba9dd56fee983
Anisa <askany07@gmail.com>
2016-06-09 18:21:17 +0000

    work is done

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,57 +1,34 @@
 'use strict';
 
 var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+//var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
 
 module.exports = function (app, passport) {
-
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
-
-	var clickHandler = new ClickHandler();
-
+	var objectDirection = {
+		url: "",
+		shortURL: ""
+	};
 	app.route('/')
-		.get(isLoggedIn, function (req, res) {
+		.get(function (req, res) {
 			res.sendFile(path + '/public/index.html');
 		});
-
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
+		
+	app.get(/(new)+/,function(req, res){
+			var initialURL = req.originalUrl.replace(/\/new\//, "");
+			if (initialURL.indexOf("http://") !== 0){
+				res.json({"error": "URL is invalid"});
+			} else {
+				var shortURL = Math.round(Math.random()*(10000-1001))+1001;
+				objectDirection.url = initialURL;
+				objectDirection.shortURL = '/' + shortURL.toString();
+				res.json({originalURL: initialURL, shortURL: req.protocol + '://' + req.hostname + '/' + shortURL});	
+			}
 		});
+	
+	app.get(/\/.+/, function(req, res){
+		if (req.originalUrl === objectDirection.shortURL){
+			res.redirect(objectDirection.url);
+		}
+	});
 
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
-
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
 };
